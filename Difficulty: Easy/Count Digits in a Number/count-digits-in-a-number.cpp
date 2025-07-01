5637696ba174c0baf2733ccf16af8427bbbb6491
Nihaal763 <nihaalkumar.cfort@gmail.com>
2025-07-01 19:24:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits in a Number/count-digits-in-a-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int countDigits(int n) {
+        // Code here
+        
+        if(n==0)
+        {
+            return 0;
+        }
+        
+        return 1+countDigits(n/10);
+    }
+};
